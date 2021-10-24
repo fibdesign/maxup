9207a3d28c0d5ee84bf676036e8fda46de66974f
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2021-10-24 07:11:34 +0330

    m

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+export default class {
+    constructor(navigation) {
+        this.navigation = document.querySelector(navigation);
+        this.navToggleBtn = this.navigation.querySelector('.nav-toggle');
+        (this.navToggleBtn)
+            ? this.SetDataCollapseOnNavToggle()
+            : console.log('MaxUp Warning : nav-toggle dose not exist!');
+
+        window.addEventListener('resize', this.SetDataCollapseOnNavToggle.bind(this));
+
+        this.navToggleBtn.addEventListener('click', this.ToggleNav.bind(this));
+    }
+    SetDataCollapseOnNavToggle(){
+        this.navToggleBtn.setAttribute('data-collapse' , (window.innerWidth <= 800));
+    }
+    ToggleNav(){
+        this.navigation.classList.toggle('navIsActive')
+    }
+}
